e003447930626af46aff96d40cebc0721c640b1c
BraydenRic <stachb@sou.edu>
2026-01-22 14:56:48 -0800

    Template looks better now

---
+++ docs/nav.js
@@ -0,0 +1,18 @@
+// Enhanced navigation with scroll effects
+(function() {
+    const header = document.querySelector('header');
+    let lastScroll = 0;
+
+    window.addEventListener('scroll', () => {
+        const currentScroll = window.pageYOffset;
+
+        // Add scrolled class when scrolled down
+        if (currentScroll > 50) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+
+        lastScroll = currentScroll;
+    });
+})();
